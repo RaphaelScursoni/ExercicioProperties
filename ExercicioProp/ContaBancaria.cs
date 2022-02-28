1e4c2d14eb04ef4c857866d759dfeb71c208ede6
Raphael Scursoni <ra.scursoni@gmail.com>
2022-02-28 20:23:07 -0300

    Solução do exercício

---
+++ ExercicioProp/ContaBancaria.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ExercicioProp
+{
+    internal class ContaBancaria
+    {
+        public int Conta { get; private set; }
+        public string Nome { get; private set; }
+        public double ValorNaConta { get; private set; }
+
+        public ContaBancaria()
+        {
+        }
+        public ContaBancaria(int conta, string nome)
+        {
+            Conta = conta;
+            Nome = nome;
+        }
+
+        public ContaBancaria (int conta, string nome, double valorNaConta) : this(conta, nome)
+        {
+            ValorNaConta = valorNaConta;
+        }
+
+        public void DepositoConta(double deposito)
+        {
+            ValorNaConta += deposito;
+        }
+        public void SaqueConta(double saque)
+        {
+            ValorNaConta = (ValorNaConta - saque) - 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta: "
+                + Conta
+                + ", "
+                + "Tirular "
+                + Nome
+                + ", "
+                + "Saldo: "
+                + ValorNaConta;
+        }
+
+
+    }
+}
---
+++ ExercicioProp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+
+namespace ExercicioProp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria conta = new ContaBancaria();
+
+            Console.Write("Entre com o número da conta:");
+            int num = int.Parse(Console.ReadLine());
+            Console.Write("Entre com o títular da conta: ");
+            string nome = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n): ");
+            string depInicial = Console.ReadLine();
+
+            if(depInicial == "s")
+            {
+                Console.Write("Digite o Valor do depósito: ");
+                double valorNaConta = double.Parse(Console.ReadLine());
+                conta = new ContaBancaria (num, nome, valorNaConta);
+            }
+            else
+            {
+                conta = new ContaBancaria(num, nome);
+            }
+
+            Console.Write("Dados da conta Bancária criada: " + conta);
+            Console.WriteLine();
+            Console.Write("Digite o Valor do depósito: ");
+            double deposito = double.Parse(Console.ReadLine());
+            conta.DepositoConta(deposito);
+            Console.Write("Dados Atualizados: " + conta);
+            Console.WriteLine();
+            Console.Write("Digite o Valor do Saque: ");
+            double saque = double.Parse(Console.ReadLine());
+            conta.SaqueConta(saque);
+            Console.Write("Dados Atualizados: " + conta);
+
+        }
+    }
+}
